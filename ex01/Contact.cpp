5ef91ff9fd465dc5adc5ec5374114a68c231ed0d
Ali Imran Bozkurt <albozkur@k2m04s02.42kocaeli.com.tr>
2024-06-30 17:25:59 +0300

    Contact get ve set methotlar

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -5,8 +5,37 @@ std::string Contact::get_first_name(void)
 	return (this->first_name);
 }
 
+std::string Contact::get_last_name(void)
+{
+	return (this->last_name);
+}
+
+std::string Contact::get_nick_name(void)
+{
+	return (this->nick_name);
+}
+
+std::string Contact::get_phone_number(void)
+{
+	return (this->pohne_number);
+}
 
 void	Contact::set_first_name(std::string name)
 {
 	this->first_name = name;
+}
+
+void	Contact::set_last_name(std::string last)
+{
+	this->last_name = last;
+}
+
+void	Contact::set_nick_name(std::string nick)
+{
+	this->nick_name = nick;
+}
+
+void	Contact::set_phone_number(std::string phone)
+{
+	this->pohne_number = phone;
 }
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -5,11 +5,17 @@ class Contact
 	private:
 		std::string first_name;
 		std::string	last_name;
-		std::string	nickname;
+		std::string	nick_name;
 		std::string	pohne_number;
 	public:
 		Contact();
 		~Contact();
 		std::string get_first_name();
+		std::string get_last_name();
+		std::string get_nick_name();
+		std::string get_phone_number();
 		void	set_first_name(std::string name);
+		void	set_last_name(std::string last);
+		void	set_nick_name(std::string nick);
+		void	set_phone_number(std::string phone);
 };
