1ac67a643ccb7aa6b571d16aa8fbe27cc84440f4
Ali Imran Bozkurt <albozkur@k2m03s05.42kocaeli.com.tr>
2024-09-08 20:38:34 +0300

    bsp

---
+++ Cpp02/ex03/Bsp.cpp
@@ -0,0 +1,36 @@
+#include "Point.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed	triangleArea(Point const a, Point const b, Point const c)
+{
+	Fixed bc = a.getX() * (b.getY() - c.getY());
+	Fixed ca = b.getX() * (c.getY() - a.getY());
+	Fixed ab = c.getX() * (a.getY() - b.getY());
+
+	if (bc < 0)
+		bc = bc * -1;
+	if (ca < 0)
+		ca = ca * -1.0f;
+	if (ab < 0)
+		ab = ab * -1.0f;
+	std::cout << bc << " 31 " << ca << " " << ab << std::endl;
+	Fixed area = (bc + ca + ab) / 2.0f;
+	return (area);
+}
+
+
+bool bsp(Point const a,Point const b,Point const c,Point const point)
+{
+	Fixed abc = triangleArea(a, b, c);
+	Fixed pbc = triangleArea(point, b, c);
+	Fixed apc = triangleArea(a, point, c);
+	Fixed abp = triangleArea(a, b, point);
+
+	std::cout << "31 " << abc << " " << pbc << " " << apc << " " << abp << std::endl;
+	if (abc == (pbc + apc + abp))
+		std::cout << "31 Ulas" << std::endl;
+	else
+		std::cout << "31 asfaf" << std::endl;
+	return (true);
+}
