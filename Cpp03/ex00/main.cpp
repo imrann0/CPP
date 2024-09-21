23e83f34a61ed575f1d8ed83f5afc700d90da6b8
Ali Imran Bozkurt <albozkur@k1m04s02.42kocaeli.com.tr>
2024-09-21 18:41:24 +0300

    Cpp03

---
+++ Cpp03/ex00/main.cpp
@@ -0,0 +1,10 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap a("ali");
+
+	a.attack("imran");
+	a.takeDamage(9);
+	a.beRepaired(8);
+}
---
+++ Cpp03/ex01/main.cpp
@@ -0,0 +1,13 @@
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+int main()
+{
+	ClapTrap a("ali");
+	ScavTrap b;
+
+	b.attack("ali");
+
+	//a.attack("imran");
+	//a.takeDamage(9);
+	//a.beRepaired(8);
+}
