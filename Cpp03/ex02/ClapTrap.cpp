c5dc9ceb80520bc742a8d25293c8ef08e8a467f6
Ali Imran Bozkurt <albozkur@k1m11s02.42kocaeli.com.tr>
2024-10-03 12:44:22 +0300

    ClapTrap Düzenleme

--- Cpp03/ex02/ClapTrap.cpp
+++ Cpp03/ex02/ClapTrap.cpp
@@ -31,6 +31,9 @@ ClapTrap::ClapTrap(ClapTrap const &trap)
 
 ClapTrap& ClapTrap::operator=(ClapTrap const &trap)
 {
+	if (&trap == this)
+		return (*this);
+
 	std::cout << "Copy Assignment Operator Called" << std::endl;
 	_name = trap._name;
 	_hit = trap._hit;
@@ -41,21 +44,21 @@ ClapTrap& ClapTrap::operator=(ClapTrap const &trap)
 
 void	ClapTrap::attack(const std::string& target)
 {
-	if (_energy > 0 && _hit >= 0)
+	if (_energy > 0 && _hit > 0)
 	{
 		std::cout << "ClapTrap " << _name << " attacks " << target << " causing "
-		<< _damage << " points of!" << std::endl;
+		<< _damage << " points of damage!" << std::endl;
 		_energy--;
 	}
-	else if (_energy == 0)
-	{
-		std::cout << "ClapTrap " << _name << " is not able to attack " << target
-		<< " because he has no energy points left." << std::endl;
-	}
-	else
+	else if (_hit <= 0)
 	{
 		std::cout << "ClapTrap " << _name << " is not able to attack " << target
 		<< " because he has not enough hit points." << std::endl;
+	}
+	else if (_energy <= 0)
+	{
+		std::cout << "ClapTrap " << _name << " is not able to attack " << target
+		<< " because he has no energy points left." << std::endl;
 	}
 }
 
@@ -70,7 +73,7 @@ void	ClapTrap::takeDamage(unsigned int amount)
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	if (_hit > 0 && _energy > 0 && _hit + amount <= 10)
+	if (_hit > 0 && _energy > 0)
 	{
 		_hit += amount;
 		std::cout << "ClapTrap " << _name << " repaired itself and gained " << amount << " of hit points, he now has " << _hit << " hit points." << std::endl;
@@ -80,6 +83,4 @@ void	ClapTrap::beRepaired(unsigned int amount)
 		std::cout << "ClapTrap " << _name << "is not able to repair itself, because he doesn't have enough energy points." << std::endl;
 	else if (_hit == 0)
 		std::cout << "ClapTrap " << _name << " is not able to repair itself, because he doesn't have enough hit points." << std::endl;
-	else
-		std::cout << "ClapTrap " << _name << " can't be repaired to have more than 10 hit points." << std::endl;
 }
